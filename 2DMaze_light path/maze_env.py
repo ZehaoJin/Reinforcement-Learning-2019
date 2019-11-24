97cf1d121a82ebf20a36440ad7bb82175c269879
Zehao Jin <50961376+ZehaoJin@users.noreply.github.com>
2019-11-24 08:55:07 -0600

    Update maze_env.py

--- 2DMaze_light path/maze_env.py
+++ 2DMaze_light path/maze_env.py
@@ -6,13 +6,8 @@ Created on Thu May 16 06:37:03 2019
 """
 
 """
-Reinforcement learning maze example.
-Red rectangle:          explorer.
-Black rectangles:       hells       [reward = -1].
-Yellow bin circle:      paradise    [reward = +1].
-All other states:       ground      [reward = 0].
-This script is the environment part of this example. The RL is in RL_brain.py.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+Modified from 2DMaze
+The agent light travels from left to right through two materials of different index of refraction.
 """
 
 
